0d896a2af4e620722215ed56ae7646fe559b9a4d
sultanmyrza@gmail.com <sultanmyrza@gmail.com>
2022-02-25 20:00:05 +0800

    define basic plugin methods

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,9 @@
 export interface PreviewVideoPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
+  previewStartFromRemote(url: string): Promise<void>;
+  previewStartFromLocal(path: string): Promise<void>;
+  previewStop(): Promise<void>;
+  playFullScreenFromRemote(url: string): Promise<void>;
+  playFullScreenFromLocal(path: string): Promise<void>;
+  stopFullScreen(): Promise<void>;
 }
--- src/web.ts
+++ src/web.ts
@@ -7,4 +7,26 @@ export class PreviewVideoWeb extends WebPlugin implements PreviewVideoPlugin {
     console.log('ECHO', options);
     return options;
   }
+  previewStartFromRemote(url: string): Promise<void> {
+    console.log(`previewStartFromRemote.url: ${url}`);
+    throw new Error('Method not implemented.');
+  }
+  previewStartFromLocal(path: string): Promise<void> {
+    console.log(`previewStartFromLocal.path: ${path}`);
+    throw new Error('Method not implemented.');
+  }
+  previewStop(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  playFullScreenFromRemote(url: string): Promise<void> {
+    console.log(`playFullScreenFromRemote.url: ${url}`);
+    throw new Error('Method not implemented.');
+  }
+  playFullScreenFromLocal(path: string): Promise<void> {
+    console.log(`playFullScreenFromLocal.path: ${path}`);
+    throw new Error('Method not implemented.');
+  }
+  stopFullScreen(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
 }
